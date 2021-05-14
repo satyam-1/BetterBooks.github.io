370e0a2831bda9217e21cc2481a5177390ab5bc4
satyam-1 <53072650+satyam-1@users.noreply.github.com>
2021-05-14 11:21:53 +0530

    Update contact.php
    commit

--- forms/contact.php
+++ forms/contact.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $subject=$_POST['subject']
 $message= $_POST['message'];
-$to = "simmigoyal50@gmail.com";
+$to = "betterbooksaa@gmail.com";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: aishfrost006@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
